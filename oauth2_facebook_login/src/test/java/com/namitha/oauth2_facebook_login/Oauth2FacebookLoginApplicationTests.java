d7d4a9d31b0a6b02f2d824873c5bacc79a539557
Namitha G S <99288233+Namitha-GS@users.noreply.github.com>
2023-05-26 06:50:13 +0530

    Add files via upload

---
+++ oauth2_facebook_login/src/test/java/com/namitha/oauth2_facebook_login/Oauth2FacebookLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namitha.oauth2_facebook_login;
+
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2FacebookLoginApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
